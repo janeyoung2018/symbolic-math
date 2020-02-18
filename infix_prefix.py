1c87c5f29e0b7d152789c5627bf17b4bb8e48ab4
janeyoung2018 <38722984+janeyoung2018@users.noreply.github.com>
2020-02-18 17:18:41 +0100

    modified infix_to_prefix function

--- infix_prefix.py
+++ infix_prefix.py
@@ -3,13 +3,7 @@
 from __future__ import division
 import random
 
-'''
-Q: Infix,  and prefix expression conversion
-For example: 
-Infix: 1 * (2 + 3) / 4
-Postfix: 1 2 3 + * 4 /
-Prefix: / * 1 + 2 3 4
-'''
+from sympy import *
 
 '''
 Fix a priority level for each operator. For example, from high to low:
@@ -18,56 +12,60 @@ Fix a priority level for each operator. For example, from high to low:
     1.    + - (subtraction)
 '''
 
-OPERATORS = set(['+', '-', '*', '/', '(', ')'])
+OPERATORS = set(['+', '-', '*', '/', '(', ')', 'pow'])
 UNARY_OPERATORS = set(["exp", "log", "sqrt", "sin", 
-                 "cos", "tan", "arcsin", "arccos", "arctan", "sinh", "cosh", "tanh", "sinh", "cosh", "tanh"])
-PRIORITY = {'+':1, '-':1, '*':2, '/':2, "exp":3, "log":3, "sqrt":3, "sin":3, 
-                 "cos":3, "tan":3, "arcsin":3, "arccos":3, "arctan":3, "sinh":3, "cosh":3, "tanh":3, "sinh":3, "cosh":3, "tanh":3}
+                 "cos", "tan", "asin", "acos", "atan", "sinh", "cosh", "tanh", "asinh", "acosh", "atanh"])
+PRIORITY = {'+':1, '-':1, '*':2, '/':2, 'pow':2, "exp":3, "log":3, "sqrt":3, "sin":3, 
+                 "cos":3, "tan":3, "asin":3, "acos":3, "atan":3, "sinh":3, "cosh":3, "tanh":3, "asinh":3, "acosh":3, "atanh":3}
 
+operator_class = ["<class 'sympy.core.add.Add'>", "<class 'sympy.core.mul.Mul'>", "<class 'sympy.core.power.Pow'>", "exp", "log",
+                 "sin", "cos", "tan", "asin", "acos", "atan", "sinh", "cosh", "tanh", "asinh", "acosh", "atanh", "Abs"]
 
+operator_dict = {"<class 'sympy.core.add.Add'>": "+", "<class 'sympy.core.mul.Mul'>": "*", "<class 'sympy.core.power.Pow'>": "pow",
+                 "exp": "exp", "log": "log", "sin": "sin", "cos": "cos", "tan": "tan", "asin": "asin", "acos": "acos", 
+                 "atan": "atan", "sinh": "sinh", "cosh": "cosh", "tanh": "tanh", "asinh": "asinh", "acosh": "acosh", "atanh": "atanh", "Abs": "abs"}
+rational_number = ["<class 'sympy.core.numbers.Rational'>", "<class 'sympy.core.numbers.Half'>"]
+exp1_number = ["<class 'sympy.core.numbers.Exp1'>"]
+invalid_function = ["<class 'sympy.core.numbers.ImaginaryUnit'>", "<class 'sympy.core.numbers.ComplexInfinity'>", 
+                    "<class 'sympy.core.numbers.NegativeInfinity'>", "<class 'sympy.core.numbers.Infinity'>",
+                   "<class 'sympy.core.numbers.NaN'>"]
+invalid_expression = ["zoo", "oo", "I", "-oo"]
 
 
 
 ### INFIX ===> PREFIX ###
-def infix_to_prefix(formula):
-    op_stack = []
-    exp_stack = []
-    exp_list = []
-    for ch in formula:
-        if ch not in OPERATORS and ch not in UNARY_OPERATORS:
-            exp_stack.append(ch)
-        elif ch in UNARY_OPERATORS:
-            op_stack.append(ch)
-        elif ch == '(':
-            op_stack.append(ch)
-        elif ch == ')':
-            while op_stack[-1] != '(':
-                generate_expression(op_stack, exp_stack) 
-            op_stack.pop() # pop '('
-        else:
-            while op_stack and op_stack[-1] != '(' and PRIORITY[ch] <= PRIORITY[op_stack[-1]]:
-                generate_expression(op_stack, exp_stack)
-            op_stack.append(ch)
-    
-    # leftover
-    while op_stack:
-        generate_expression(op_stack, exp_stack)
-    print(exp_stack[-1])
-    print(exp_stack[-1].split())
-    return exp_stack[-1], exp_stack[-1].split()
-
-def generate_expression(op_stack, exp_stack):
-    op = op_stack.pop()
-    if op in OPERATORS:
-        a = exp_stack.pop()
-        b = exp_stack.pop()
-        expression = op + " " + b + " " + a
-        exp_stack.append(expression)
+x = symbols('x')
+def infix_to_prefix(expr, seq=None):
+    if not seq:
+        seq = []
+    s1 = str(expr)
+    s2 = str(expr.func)
+    if s1 in invalid_expression or s2 in invalid_function:
+        return False
+    length = len(expr.args)
+    if s2 in operator_class:
+        op = operator_dict[s2]
+        seq.append(op)
+    elif s2 in rational_number:
+        s1 = s1.split("/")
+        seq.append("/")
+        seq.append(s1[0])
+        seq.append(s1[1])
+    elif s2 in exp1_number:
+        seq.append("exp")
+        seq.append("1")
     else:
-        a = exp_stack.pop()
-        expression = op + " " + a
-        exp_stack.append(expression)
-
+        seq.append(s1)
+            
+    i = 0
+    while i < length:
+        if i!=0 and i!=(length-1):
+            seq.append(op)
+        arg = expr.args[i]
+        infix_to_prefix(arg, seq)
+        i += 1
+    return seq   
+        
 ### PREFIX ===> INFIX ###
 '''
 Scan the formula reversely
@@ -87,20 +85,20 @@ def prefix_to_infix(formula):
                 if prev_op and PRIORITY[prev_op] < PRIORITY[ch]:
                     exp = '(' + " " + a + " " + ')' + " " + ch + " "+ b
                 else:
-                    exp = a + " " + ch+ " " + b
+                    exp = a + " " + ch + " " + b
             else:
                 a = stack.pop()
                 exp = ch + " " + "(" + " " +  a + " " +")"
             stack.append(exp)
             prev_op = ch
-    print(stack[-1])
-    print(stack[-1].split())
-    return stack[-1], stack[-1].split()
+    #print(stack[-1])
+    #print(stack[-1].split())
+    return stack[-1].split()
 
 
 
 
 
 if __name__ == '__main__':
-    infix_to_prefix(['cosh', '(', '(', '4', '-', 'sinh', '(', '(', '-4', '/', '-2', ')', ')', ')', ')'])
+    infix_to_prefix(simplify(sympify("log(exp(x))")))
     prefix_to_infix(['cosh', '-', '4', 'sinh', '/', '-4', '-2'])
